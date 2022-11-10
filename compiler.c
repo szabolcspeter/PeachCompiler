7972aa5337a58461579d3374425eee50b4ff6b49
Szabolcs Peter <szabi.peter@gmail.com>
2022-11-10 15:36:26 +0100

    Initial setup

---
+++ compiler.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "compiler.h"
+
+int compile_file(const char* filename, const char* out_filename, int flags)
+{
+    struct compile_process* process = compile_process_create(filename, out_filename, flags);
+    if (!process)
+    {
+        return COMPILER_FAILED_WITH_ERRORS;
+    }
+
+    // Preform lexical analysis
+
+    // Preform parsing
+
+    // Preform code generation
+
+
+    fclose(process->ofile);
+    fclose(process->cfile.fp);
+    free(process);
+
+    return COMPILER_FILE_COMPILED_OK;
+}
